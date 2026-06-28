519ff6435af9b0d357cc32e3b84e135707bc90a7
mahmutovicr <mahmutovicrahman5@gmail.com>
2026-06-29 01:23:07 +0200

    add SSG pre-rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { ViteReactSSG } from 'vite-react-ssg'
 import App from './App'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+export const createRoot = ViteReactSSG({
+  routes: [{ path: '/', element: <App /> }],
+})
